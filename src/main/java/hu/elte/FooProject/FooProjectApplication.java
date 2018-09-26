924e50c3b33ce1f10cdef506fd146802562a5839
Madhh <43617026+Madhh@users.noreply.github.com>
2018-09-26 20:33:30 +0200

    Add files via upload

---
+++ src/main/java/hu/elte/FooProject/FooProjectApplication.java
@@ -0,0 +1,25 @@
+
+package hu.elte.FooProject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class FooProjectApplication extends WebMvcConfigurerAdapter {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(FooProjectApplication.class, args);
+    }
+
+
+}
